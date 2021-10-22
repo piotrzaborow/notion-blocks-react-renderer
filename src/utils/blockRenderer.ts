8327118d2a459543d3ccdb1f549d39b45f84e4dd
Piotr Zaborowski <piotrzaborow@gmail.com>
2021-10-22 16:50:06 +0200

    refactor: delete unused console.logs

--- src/utils/blockRenderer.ts
+++ src/utils/blockRenderer.ts
@@ -2,7 +2,6 @@ import { appendKeyToElement } from "./appendKeyToElement"
 import { IBlock, RenderNode } from "./UtilsInterfaces"
 
 const blockRenderer = (block: IBlock, renderers?: RenderNode) => {
-	// console.log(block.type)
 	return appendKeyToElement(renderers[block.type](block), block.id)
 }
 
